ac20b964e4360e8a68faec98e9f5c0983ffd9bb6
rabe12321 <104843218+rabe12321@users.noreply.github.com>
2023-06-13 09:42:12 +0200

    - eigene Klassen für Checkbox und Selectbox - Änderungen an den Grafiken - Allgemein weiter gearbeitet

---
+++ GUI/gui_elements.py
@@ -0,0 +1,117 @@
+from filepath import FILEPATH
+from PIL import Image, ImageTk
+import tkinter.ttk as ttk
+
+
+class CheckBox(ttk.Label):
+    def __init__(self, frame, size_x, size_y, style, selected, command_select, command_unselect):
+        self.__checkbox_selected = Image.open(FILEPATH + "Checkbox_selected.png")
+        self.__checkbox_selected = self.__checkbox_selected.resize((size_x, size_y))
+        self.__checkbox_selected_foto = ImageTk.PhotoImage(self.__checkbox_selected)
+
+        self.__checkbox_unselected = Image.open(FILEPATH + "Checkbox_unselected.png")
+        self.__checkbox_unselected = self.__checkbox_unselected.resize((size_x, size_y))
+        self.__checkbox_unselected_foto = ImageTk.PhotoImage(self.__checkbox_unselected)
+
+        self.__command_select = command_select
+        self.__command_unselect = command_unselect
+
+        super(CheckBox, self).__init__(frame,
+                                       image=self.__checkbox_unselected_foto,
+                                       style=style)
+
+        self.__is_selected = selected
+
+        if self.__is_selected:
+            self.config(image=self.__checkbox_selected_foto)
+            self.__is_selected = True
+
+        self.bind("<Button-1>", self.click)
+
+    def click(self, event):
+        if self.__is_selected:
+            self.unselect(self.__command_unselect)
+        else:
+            self.select(self.__command_select)
+
+    def select(self, command):
+        self.config(image=self.__checkbox_selected_foto)
+        self.__is_selected = True
+        if command is None:
+            pass
+        else:
+            command()
+
+    def unselect(self, command):
+        self.config(image=self.__checkbox_unselected_foto)
+        self.__is_selected = False
+        if command is None:
+            pass
+        else:
+            command()
+
+    def get_selected(self):
+        return self.__is_selected
+
+    def resize(self, size_x, size_y):
+        self.__checkbox_selected = self.__checkbox_selected.resize((size_x, size_y))
+        self.__checkbox_selected_foto = ImageTk.PhotoImage(self.__checkbox_selected)
+        self.__checkbox_unselected = self.__checkbox_unselected.resize((size_x, size_y))
+        self.__checkbox_unselected_foto = ImageTk.PhotoImage(self.__checkbox_unselected)
+
+
+class SelectBox(ttk.Label):
+    def __init__(self, frame, size_x, size_y, style, selected, command_select, command_unselect):
+        self.__selectbox_selected = Image.open(FILEPATH + "selectbox_selected.png")
+        self.__selectbox_selected = self.__selectbox_selected.resize((size_x, size_y))
+        self.__selectbox_selected_foto = ImageTk.PhotoImage(self.__selectbox_selected)
+
+        self.__selectbox_unselected = Image.open(FILEPATH + "selectbox_unselected.png")
+        self.__selectbox_unselected = self.__selectbox_unselected.resize((size_x, size_y))
+        self.__selectbox_unselected_foto = ImageTk.PhotoImage(self.__selectbox_unselected)
+
+        self.__command_select = command_select
+        self.__command_unselect = command_unselect
+
+        super(CheckBox, self).__init__(frame,
+                                       image=self.__selectbox_unselected_foto,
+                                       style=style)
+
+        self.__is_selected = selected
+
+        if self.__is_selected:
+            self.config(image=self.__selectbox_selected_foto)
+            self.__is_selected = True
+
+        self.bind("<Button-1>", self.click)
+
+    def click(self, event):
+        if self.__is_selected:
+            self.unselect(self.__command_unselect)
+        else:
+            self.select(self.__command_select)
+
+    def select(self, command):
+        self.config(image=self.__selectbox_selected_foto)
+        self.__is_selected = True
+        if command is None:
+            pass
+        else:
+            command()
+
+    def unselect(self, command):
+        self.config(image=self.__selectbox_unselected_foto)
+        self.__is_selected = False
+        if command is None:
+            pass
+        else:
+            command()
+
+    def get_selected(self):
+        return self.__is_selected
+
+    def resize(self, size_x, size_y):
+        self.__selectbox_selected = self.__selectbox_selected.resize((size_x, size_y))
+        self.__selectbox_selected_foto = ImageTk.PhotoImage(self.__selectbox_selected)
+        self.__selectbox_unselected = self.__selectbox_unselected.resize((size_x, size_y))
+        self.__selectbox_unselected_foto = ImageTk.PhotoImage(self.__selectbox_unselected)
--- GUI/main.py
+++ GUI/main.py
@@ -3,15 +3,17 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import messagebox
 from PIL import Image, ImageTk
-from filepath import *
 import os
+import gui_elements as gui
+import sys
+
+from filepath import FILEPATH
 
 # %% -----------------Farben definieren--------------------------------------------------------------------------------#
 # col_Blue = '#%02x%02x%02x' % (1, 105, 196)  # RGB values
 col_Blue = '#%02x%02x%02x' % (18, 123, 202)  # RGB values TEC blue
 col_Grey = '#%02x%02x%02x' % (200, 200, 200)  # RGB values
 col_GlobalBackground = '#%02x%02x%02x' % (200, 200, 200)  # RGB light grey
-
 
 # %% -----------------Hauptfenster erstellen---------------------------------------------------------------------------#
 root = tk.Tk()
@@ -34,21 +36,38 @@ hsrtLogoFoto = ImageTk.PhotoImage(hsrtLogo)
 buttonStart = Image.open(FILEPATH + "Buttonstart.png")
 buttonStartFoto = ImageTk.PhotoImage(buttonStart)
 
-buttonStartPress = Image.open(FILEPATH + "Buttonstart_ausgefüllt.png")
+buttonStartPress = Image.open(FILEPATH + "Buttonstart_ausgefuellt.png")
 buttonStartPressFoto = ImageTk.PhotoImage(buttonStartPress)
 
-weizen = Image.open(FILEPATH + "weizenFrame.png") # TODO copyright?!
+weizen = Image.open(FILEPATH + "weizenFrame.png")  # TODO copyright?!
 weizenFoto = ImageTk.PhotoImage(weizen)
 
-cola = Image.open(FILEPATH + "colaFrame.png") # TODO copyright?!
+cola = Image.open(FILEPATH + "colaFrame.png")  # TODO copyright?!
 colaFoto = ImageTk.PhotoImage(cola)
+
+rectTop = Image.open(FILEPATH + "rectTop.png")
+rectTopFoto = ImageTk.PhotoImage(rectTop)
+
+glasPosBox = Image.open(FILEPATH + "glasPosBox.png")
+glasPosBoxFoto = ImageTk.PhotoImage(glasPosBox)
+
+glasPosBoxHide = Image.open(FILEPATH + "glasPosBoxHide.png")
+glasPosBoxHideFoto = ImageTk.PhotoImage(glasPosBoxHide)
+
+selectboxSelected = Image.open(FILEPATH + "selectbox_selected.png")
+selectboxSelected = selectboxSelected.resize((80, 80))
+selectboxSelectedFoto = ImageTk.PhotoImage(selectboxSelected)
+
+selectboxUnselected = Image.open(FILEPATH + "selectbox_unselected.png")
+selectboxUnselected = selectboxUnselected.resize((80, 80))
+selectboxUnselectedFoto = ImageTk.PhotoImage(selectboxUnselected)
 
 # %% -----------------Styles konfigurieren-----------------------------------------------------------------------------#
 style = ttk.Style()
-#style.theme_use('clam')
+# style.theme_use('clam')
 
 # ttk azure theme anwenden: für grundlegendes Design, danach individuelle Anpassungen
-root.tk.call("source", "azure.tcl")
+root.tk.call("source", FILEPATH + "..\\azure.tcl")
 root.tk.call("set_theme", "light")
 
 # alle Pages Hintergrundfarbe
@@ -62,39 +81,53 @@ style.configure("BW.TLabel",
                 font=("Segoe UI", 25))
 
 # Label Hintergrund wie alle Pages
-style.configure("invisible_label.TLabel",
-                background=col_GlobalBackground)
+style_invisible_label = ttk.Style()
+style_invisible_label.configure("invisible_label.TLabel",
+                                background=col_GlobalBackground)
 
 # Button allgemein
 style.configure("TButton",
                 font=("Arial", 30))
 
-#Start-Label
+# Start-Label
 style.configure("labelStart.TLabel",
                 font=(None, 40),
                 background=col_GlobalBackground,
                 foreground='white'
                 )
-'''
+
 # Start-Button
-style.configure("buttonStart.TButton",
-                font=("Arial", 40, "bold"),
-                foreground=col_GlobalBackground,
-                background=col_GlobalBackground,
-                borderwidth=0,
-                relief='none')
-'''
+styleButtonStart = ttk.Style()
+styleButtonStart.configure("buttonStart.TButton",
+                           font=("Arial", 40, "bold"),
+                           activeforeground="blue",
+                           activebackground="red",
+                           borderwidth=0,
+                           relief='none')
 
 # %% -----------------Pages/Frames erstellen---------------------------------------------------------------------------#
 page1 = ttk.Frame(root)  # Startseite
 page2 = ttk.Frame(root)  # Auswahl Getränk
 page3 = ttk.Frame(root)  # Bierglas reinstellen und Lichtschranke schalten und bestätigen
 
+# %% -----------------Globale Variablen---------------------------------------------------------------------------------#
+cola_selected = False
+weizen_selected = False
+glas_pos_selected = False
+
 
 # %% -----------------Funktionen---------------------------------------------------------------------------------------#
 def showFrame(frame):
     frame.update()
     frame.tkraise()
+
+
+def show_widget(frame):
+    frame.place()
+
+
+def hide_widget(frame):
+    frame.place_forget()
 
 
 def helpPage():  # TODO Help-Page bauen, evtl. pdf o. ä.
@@ -127,6 +160,37 @@ def show_labelStart_release(event):
     labelStart.configure(image=buttonStartFoto)
 
 
+def cola_select():
+    global cola_selected
+    cola_selected = True
+    if checkbox_weizen.get_selected():
+        checkbox_weizen.click(checkbox_weizen.event_info())
+
+
+def cola_unselect():
+    global cola_selected
+    cola_selected = False
+
+
+def weizen_select():
+    global weizen_selected
+    weizen_selected = True
+    if checkbox_cola.get_selected():
+        checkbox_cola.click(checkbox_cola.event_info())
+
+
+def weizen_unselect():
+    global weizen_selected
+    weizen_selected = False
+
+
+def toggle_glaspos_selected(event):
+    global glas_pos_selected
+    glas_pos_selected = not glas_pos_selected
+    if glas_pos_selected:
+        selectBoxGlas.config(image=selectboxSelectedFoto)
+    else:
+        selectBoxGlas.config(image=selectboxUnselectedFoto)
 
 
 # %% -----------------Popup-Fenster------------------------------------------------------------------------------------#
@@ -180,7 +244,8 @@ for frame in (page1, page2, page3):
                       image=hsrtLogoFoto,
                       borderwidth=0,
                       style="invisible_label.TLabel")
-    label.place(x=1268, y=30)
+    label.place(x=1268,
+                y=30)
 
 showFrame(page1)
 
@@ -196,7 +261,8 @@ Willkommen_label.place(x=0,
                        width=1920,
                        height=300)
 
-labelStart = ttk.Label(page1, # TODO bind das Label, image ändern wenn Maus drüber, nächste Seite aufrufen mit showFrame() wenn geklickt
+labelStart = ttk.Label(page1,
+                       # TODO bind das Label, image ändern wenn Maus drüber, nächste Seite aufrufen mit showFrame() wenn geklickt
                        image=buttonStartFoto,
                        text="Start",
                        compound="center",
@@ -208,8 +274,6 @@ labelStart.place(relx=0.5,
 labelStart.bind("<ButtonPress>", show_labelStart_press)
 labelStart.bind("<ButtonRelease>", show_labelStart_release)
 
-
-'''
 buttonStart = ttk.Button(page1,
                          image=buttonStartFoto,
                          text="Start",
@@ -219,20 +283,56 @@ buttonStart = ttk.Button(page1,
 buttonStart.place(relx=0.5,
                   y=850,
                   anchor='center')
-'''
+
+
+def commandselect():
+    print("commandselect")
+
+
+def commandunselect():
+    print("commandunselect")
+
+
+checkboxtest = gui.CheckBox(page1, 500, 500, "invisible_label.TLabel", True, commandselect, None)
+checkboxtest.place(x=100, y=100)
+
 # %% -----------------page2--------------------------------------------------------------------------------------------#
 labelWeizen = ttk.Label(page2,
                         image=weizenFoto,
                         style="invisible_label.TLabel")
 labelWeizen.place(x=30,
-                  y=250)
+                  y=265)
 
 labelCola = ttk.Label(page2,
                       image=colaFoto,
                       style="invisible_label.TLabel")
 labelCola.place(x=320,
-                y=250)
+                y=265)
 
+checkbox_weizen = gui.CheckBox(labelWeizen, 80, 80, "invisible_label.TLabel", False, weizen_select, weizen_unselect)
+checkbox_weizen.place(x=90, y=590)
+
+checkbox_cola = gui.CheckBox(labelCola, 80, 80, "invisible_label.TLabel", False, cola_select, cola_unselect)
+checkbox_cola.place(x=90, y=590)
+
+glasPosBoxFrame = ttk.Label(page2,
+                            image=glasPosBoxFoto,
+                            style="invisible_label.TLabel")
+glasPosBoxFrame.place(x=610,
+                      y=265)
+
+glasPosBoxHideFrame = ttk.Label(page2,
+                                image=glasPosBoxHideFoto,
+                                style="invisible_label.TLabel")
+glasPosBoxHideFrame.place(x=610,
+                          y=265)
+
+selectBoxGlas = ttk.Label(glasPosBoxFrame,
+                          image=selectboxUnselectedFoto,
+                          style="invisible_label.TLabel")
+selectBoxGlas.place(x=583,
+                    y=410)
+selectBoxGlas.bind("<Button-1>", toggle_glaspos_selected)
 
 # %% -----------------page3--------------------------------------------------------------------------------------------#
 buttonStartBier = ttk.Button(page3, text="Bierglas steht drin",
