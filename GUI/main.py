874535dba4987d2df85745cd252fb31f66beb900
rabe12321 <104843218+rabe12321@users.noreply.github.com>
2023-09-21 14:57:48 +0200

    changes

--- GUI/main.py
+++ GUI/main.py
@@ -436,14 +436,14 @@ Willkommen_label = ttk.Label(page1,
                              anchor='center',
                              background=col_Grey,
                              foreground='white')
-Willkommen_label.place(x=0,
+Willkommen_label.place(x=-400,
                        y=390,
                        width=1920,
                        height=300)
 
 
 buttonStart = gui_elements.StartButton(page1, 800, 630, "invisible_label.TLabel", None, show_page2)
-buttonStart.place(relx=0.67,
+buttonStart.place(relx=0.7,
                   y=600,
                   anchor='center')
 # %% -----------------page2--------------------------------------------------------------------------------------------#
